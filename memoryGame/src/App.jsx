993b8ec2cd30990c26c2cf2c3112e017b2014aa0
Grzegorz <gkopacki92@gmail.com>
2024-01-25 13:45:25 +0100

    created utils folder and Items.js file and added function for shuffling array elements

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -1,25 +1,10 @@
 import styles from "./App.module.css";
 import { Item } from "./components/Item/Item";
+import { shuffleArray } from "./utils/shuffleArray";
+import { Items } from "./utils/Items";
 
 function App() {
-	const Items = [
-		{ name: "1", id: 1 },
-		{ name: "1", id: 2 },
-		{ name: "2", id: 3 },
-		{ name: "2", id: 4 },
-		{ name: "3", id: 5 },
-		{ name: "3", id: 6 },
-		{ name: "4", id: 7 },
-		{ name: "4", id: 8 },
-		{ name: "5", id: 9 },
-		{ name: "5", id: 10 },
-		{ name: "6", id: 12 },
-		{ name: "6", id: 12 },
-		{ name: "7", id: 13 },
-		{ name: "7", id: 14 },
-		{ name: "8", id: 15 },
-		{ name: "8", id: 16 },
-	];
+	const shuffledItems = shuffleArray(Items);
 
 	return (
 		<div className={styles.container}>
@@ -27,7 +12,7 @@ function App() {
 
 			<h2 className={styles.playerMove}>Ruch gracza 1</h2>
 			<div className={styles.itemsBox}>
-				{Items.map(({ name, id }) => (
+				{shuffledItems.map(({ name, id }) => (
 					<Item name={name} key={id} />
 				))}
 			</div>
