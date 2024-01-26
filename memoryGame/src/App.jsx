aa6d7b43180e7c781f92a8b7884615d315f5aa91
Grzegorz <gkopacki92@gmail.com>
2024-01-26 22:50:31 +0100

    changed value for keys in Items array

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -4,25 +4,6 @@ import { Item } from "./components/Item/Item";
 import { shuffleArray } from "./utils/shuffleArray";
 import { Items } from "./utils/Items";
 
-// const Items = [
-// 	{ name: "1", id: 1, key: 10 },
-// 	{ name: "1", id: 1, key: 20 },
-// 	{ name: "2", id: 2, key: 30 },
-// 	{ name: "2", id: 2, key: 40 },
-// 	{ name: "3", id: 3, key: 50 },
-// 	{ name: "3", id: 3, key: 60 },
-// 	{ name: "4", id: 4, key: 70 },
-// 	{ name: "4", id: 4, key: 80 },
-// 	{ name: "5", id: 5, key: 90 },
-// 	{ name: "5", id: 5, key: 100 },
-// 	{ name: "6", id: 6, key: 110 },
-// 	{ name: "6", id: 6, key: 120 },
-// 	{ name: "7", id: 7, key: 130 },
-// 	{ name: "7", id: 7, key: 140 },
-// 	{ name: "8", id: 8, key: 150 },
-// 	{ name: "8", id: 8, key: 160 },
-// ];
-
 function App() {
 	const [itemShown, setItemShown] = useState(Array(Items.length).fill(false));
 	const [clickedItems, setClickedItems] = useState([]);
