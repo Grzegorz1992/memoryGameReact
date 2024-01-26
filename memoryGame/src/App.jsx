84912fd78f3310c9cdf9dc42444df297d400bfdb
Grzegorz <gkopacki92@gmail.com>
2024-01-26 22:48:19 +0100

    created Items.js file to hold an array with game items

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -2,46 +2,44 @@ import { useState, useMemo } from "react";
 import styles from "./App.module.css";
 import { Item } from "./components/Item/Item";
 import { shuffleArray } from "./utils/shuffleArray";
+import { Items } from "./utils/Items";
 
-const Items = [
-	{ name: "1", id: 1, key: 10 },
-	{ name: "1", id: 1, key: 20 },
-	{ name: "2", id: 2, key: 30 },
-	{ name: "2", id: 2, key: 40 },
-	{ name: "3", id: 3, key: 50 },
-	{ name: "3", id: 3, key: 60 },
-	{ name: "4", id: 4, key: 70 },
-	{ name: "4", id: 4, key: 80 },
-	{ name: "5", id: 5, key: 90 },
-	{ name: "5", id: 5, key: 100 },
-	{ name: "6", id: 6, key: 110 },
-	{ name: "6", id: 6, key: 120 },
-	{ name: "7", id: 7, key: 130 },
-	{ name: "7", id: 7, key: 140 },
-	{ name: "8", id: 8, key: 150 },
-	{ name: "8", id: 8, key: 160 },
-];
+// const Items = [
+// 	{ name: "1", id: 1, key: 10 },
+// 	{ name: "1", id: 1, key: 20 },
+// 	{ name: "2", id: 2, key: 30 },
+// 	{ name: "2", id: 2, key: 40 },
+// 	{ name: "3", id: 3, key: 50 },
+// 	{ name: "3", id: 3, key: 60 },
+// 	{ name: "4", id: 4, key: 70 },
+// 	{ name: "4", id: 4, key: 80 },
+// 	{ name: "5", id: 5, key: 90 },
+// 	{ name: "5", id: 5, key: 100 },
+// 	{ name: "6", id: 6, key: 110 },
+// 	{ name: "6", id: 6, key: 120 },
+// 	{ name: "7", id: 7, key: 130 },
+// 	{ name: "7", id: 7, key: 140 },
+// 	{ name: "8", id: 8, key: 150 },
+// 	{ name: "8", id: 8, key: 160 },
+// ];
 
 function App() {
 	const [itemShown, setItemShown] = useState(Array(Items.length).fill(false));
 	const [clickedItems, setClickedItems] = useState([]);
 
 	function handleItemClick(index) {
-		
 		if (clickedItems.length < 2 && !clickedItems.includes(index)) {
 			const newItemShown = [...itemShown];
 			newItemShown[index] = true;
 			setItemShown(newItemShown);
 
-			
 			setClickedItems((prevClickedItems) => [...prevClickedItems, index]);
 
-			
 			if (clickedItems.length + 1 === 2) {
 				setTimeout(() => {
 					setItemShown(Array(Items.length).fill(false));
 					setClickedItems([]);
-				}, 1000); 
+				}, 1000);
 			}
 		}
 	}
--- memoryGame/src/components/Item/Item.jsx
+++ memoryGame/src/components/Item/Item.jsx
@@ -1,17 +1,4 @@
-// import styles from "./Item.module.css";
-// import { useState } from "react";
-
 export function Item({ name, id, onClick, className }) {
-	// const [itemShown, setItemShown] = useState(false);
-	// const [numbers, setNumbers] = useState([{ name: name, id: id }]);
-
-	// function handleItemClick() {
-	// 	setNumbers((prevNumber) => [...prevNumber, { name: name, id: id }]);
-
-	// 	setItemShown(true);
-	// 	console.log(numbers);
-	// }
-
 	return (
 		<div onClick={onClick} className={className} id={id}>
 			{name}
