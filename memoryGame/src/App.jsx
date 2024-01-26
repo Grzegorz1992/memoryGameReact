fa8feb382cb9c0a0b3ac3a39da4e13c6963210ca
Grzegorz <gkopacki92@gmail.com>
2024-01-26 22:45:25 +0100

    added state to handle initial game logic

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -1,58 +1,65 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import styles from "./App.module.css";
 import { Item } from "./components/Item/Item";
 import { shuffleArray } from "./utils/shuffleArray";
 
+const Items = [
+	{ name: "1", id: 1, key: 10 },
+	{ name: "1", id: 1, key: 20 },
+	{ name: "2", id: 2, key: 30 },
+	{ name: "2", id: 2, key: 40 },
+	{ name: "3", id: 3, key: 50 },
+	{ name: "3", id: 3, key: 60 },
+	{ name: "4", id: 4, key: 70 },
+	{ name: "4", id: 4, key: 80 },
+	{ name: "5", id: 5, key: 90 },
+	{ name: "5", id: 5, key: 100 },
+	{ name: "6", id: 6, key: 110 },
+	{ name: "6", id: 6, key: 120 },
+	{ name: "7", id: 7, key: 130 },
+	{ name: "7", id: 7, key: 140 },
+	{ name: "8", id: 8, key: 150 },
+	{ name: "8", id: 8, key: 160 },
+];
+
 function App() {
-	const Items = [
-		{ name: "1", id: 1, key: 10 },
-		{ name: "1", id: 1, key: 20 },
-		{ name: "2", id: 2, key: 30 },
-		{ name: "2", id: 2, key: 40 },
-		{ name: "3", id: 3, key: 50 },
-		{ name: "3", id: 3, key: 60 },
-		{ name: "4", id: 4, key: 70 },
-		{ name: "4", id: 4, key: 80 },
-		{ name: "5", id: 5, key: 90 },
-		{ name: "5", id: 5, key: 100 },
-		{ name: "6", id: 6, key: 110 },
-		{ name: "6", id: 6, key: 120 },
-		{ name: "7", id: 7, key: 130 },
-		{ name: "7", id: 7, key: 140 },
-		{ name: "8", id: 8, key: 150 },
-		{ name: "8", id: 8, key: 160 },
-	];
+	const [itemShown, setItemShown] = useState(Array(Items.length).fill(false));
+	const [clickedItems, setClickedItems] = useState([]);
 
-	const [numbers, setNumbers] = useState([{name:name}]);
-	const [itemShown, setItemShown] = useState(false);
+	function handleItemClick(index) {
+		
+		if (clickedItems.length < 2 && !clickedItems.includes(index)) {
+			const newItemShown = [...itemShown];
+			newItemShown[index] = true;
+			setItemShown(newItemShown);
 
-	function handleItemClick() {
-		setNumbers((prevNumber) => [...prevNumber, {name:name  }]);
+			
+			setClickedItems((prevClickedItems) => [...prevClickedItems, index]);
 
-		setItemShown(true);
-		console.log(numbers);
+			
+			if (clickedItems.length + 1 === 2) {
+				setTimeout(() => {
+					setItemShown(Array(Items.length).fill(false));
+					setClickedItems([]);
+				}, 1000); 
+			}
+		}
 	}
 
-	const shuffledItems = shuffleArray(Items);
+	const shuffledItems = useMemo(() => shuffleArray(Items), []);
 
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.header}>MEMORY NUMBERS</h1>
 			<h2 className={styles.playerMove}>Ruch gracza 1</h2>
 			<div className={styles.itemsBox}>
-				{shuffledItems.map(({ name, key, id }) => (
+				{shuffledItems.map(({ name, key, id }, index) => (
 					<Item
-						name={itemShown && numbers.length <= 2 ? name : name}
+						name={itemShown[index] ? name : "?"}
 						key={key}
 						id={id}
-						className={
-							itemShown && numbers.length <= 2
-								? styles.item
-								: !itemShown
-								? styles.item
-								: styles.hideItem
-						}
-						onClick={() => handleItemClick(key)}
+						className={itemShown[index] ? styles.item : styles.hideItem}
+						onClick={() => handleItemClick(index)}
 					></Item>
 				))}
 			</div>
@@ -63,5 +70,3 @@ function App() {
 }
 
 export default App;
-
-
